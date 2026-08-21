d0495015d538bffb93ed09a2163674393cab35fd
kaironium <kai.svenson628@gmail.com>
2026-08-21 11:11:33 -0500

    edits to projected computation and funcs

--- projected_computation.py
+++ projected_computation.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 import dynamite as dm
 import dynamite.tools as tools
 import dynamite.states as st
-import dynamite.computations as comp
 
 import utilities as ut
 import hamiltonian_factory as hf
+import projected_funcs as pf
 
 
 if __name__ == '__main__':
@@ -19,24 +19,29 @@ if __name__ == '__main__':
     size = comm.Get_size()
 
     # SYK Parameters
-    N = 12   # total number of fermions
+    N = 20   # total number of fermions
     NA = 2   # number of fermions in subsystem
     g = 1    # coupling to 2-body interactions
     dr = 10  # dr stands for disorder realizations
 
     # RNG config
     base_seed = 628**3  # the better circle constant!
-    rng = [np.random.SeedSequence(base_seed, spawn_key=(r,)) for r in range(dr)]
+    rng = lambda r: np.random.default_rng(np.random.SeedSequence(base_seed, spawn_key=(r,)))
     H_title = 'SYK42'
     H_str = lambda r: f'{H_title}_N{N}_g{g}_base-seed{base_seed}_r{r}'
     H_func = hf.SYK42
-    H_args = lambda r: [g, rng[r]]
+    H_args = lambda r: [g, rng(r)]
+
+    # dynamite config
+    dm.config.L = N // 2
+    D = 2**dm.config.L
+    DA = 2**(NA // 2)
 
     # Experiment Parameters
     psi0_str = '0'
     psi0 = st.State(0)
-    k_series = np.arange(2) + 1
-    log_tmax = 8
+    k_series = np.arange(3) + 1
+    log_tmax = 3
     ns = 100
     tau_series = np.logspace(0, log_tmax, num=ns)
     exp_note = lambda r, k: f'{H_str(r)}_psi0{psi0_str}_k{k}_logtmax{log_tmax}_ns{ns}'
@@ -47,66 +52,38 @@ if __name__ == '__main__':
         f'Base Seed: {base_seed}',
         rf'$|\psi_0\rangle$: {psi0_str}',
     ))
-
-    # dynamite config
-    dm.config.L = N // 2
-    D = 2**dm.config.L
-    DA = 2**(NA // 2)
-
-    proj_1norm = np.empty((dr,) + k_series.shape + tau_series.shape)
-    proj_2norm = np.empty_like(proj_1norm)
+    
+    # Computations
+    proj_norm = np.empty((2, dr,) + k_series.shape + tau_series.shape)
     for r in range(dr):  # TODO: make this loop into a slurm job array
+        print(f'Realization {r+1}/{dr}')
         H = H_func(*(H_args(r)))
-        psit = np.empty((tau_series.size, D))
-        for i, tau in enumerate(tau_series):
-            psit[i, :] = comp.evolve(H, psi0, tau).to_numpy(to_all=True)
-        psit = psit.reshape(tau_series.size, -1, DA)
+        pm = pf.proj_moment(H, psi0, tau_series, k_series, DA, note=exp_note(r, f'{k_series[0]}-{k_series[-1]}'))
+        # TODO: here we would insert the computation of the scrooge ensemble
         for k_idx, k in enumerate(k_series):
-            # TODO: post-select on parity
-            # TODO: proper normalization
-            moment = np.einsum('abc,abd->acd', psit, psit.conj(), optimize=True)
-
-            # uncomment if we need to chunk:
-            # chunk = 256
-            # for c in range(0, psit.shape[1], chunk):  # chunk over system B size
-            #     moment = np.einsum('abc,abd->acd', psit[:, c:c+chunk, :], psit[:, c:c+chunk, :].conj())
-
-            # TODO: here we would insert the computation of the scrooge ensemble
-            proj_1norm[r, k_idx] = np.linalg.norm(moment, ord='nuc', axis=(1, 2))
-            proj_2norm[r, k_idx] = np.linalg.norm(moment, ord='fro', axis=(1, 2))
-    proj_1norm = np.mean(proj_1norm, axis=0)
-    proj_2norm = np.mean(proj_2norm, axis=0)
-    proj_1norm_sem = np.std(proj_1norm, ddof=1, axis=0) / np.sqrt(dr)
-    proj_2norm_sem = np.std(proj_2norm, ddof=1, axis=0) / np.sqrt(dr)
-
+            for p in range(2):
+                proj_norm[p, r, k_idx] = pf.norm(pm[str(k)], p+1, axis=(1, 2))
+    proj_norm_mean = np.mean(proj_norm, axis=1)
+    proj_norm_sem = np.std(proj_norm, ddof=1, axis=1) / np.sqrt(dr)
+    
+    # Plotting
     fig, ax = plt.subplots(1, 2, figsize=ut.fig_size(2, 1))
-    for k_idx, k in enumerate(k_series):
-        ax[0].plot(tau_series, proj_1norm[k_idx], label=rf'$k={k}$')
-        ax[1].plot(tau_series, proj_2norm[k_idx], label=rf'$k={k}$')
-        ax[0].fill_between(
-            tau_series,
-            proj_1norm[k_idx] - proj_1norm_sem[k_idx],
-            proj_1norm[k_idx] + proj_1norm_sem[k_idx],
-            alpha=0.5
+    for p in range(2):
+        for k_idx, k in enumerate(k_series):
+            ax[p].plot(tau_series, proj_norm_mean[p, k_idx], label=rf'$k={k}$')
+            ax[p].fill_between(
+                tau_series,
+                proj_norm_mean[p, k_idx] - proj_norm_sem[p, k_idx],
+                proj_norm_mean[p, k_idx] + proj_norm_sem[p, k_idx],
+                alpha=0.5
+            )
+        ax[p].set(
+            xlabel=r'$\tau$',
+            ylabel=r'$||\rho_\text{Proj.}^{(k)}||_' + f'{p+1}$',
+            xscale='log',
+            title=rf'{p+1}-norm'
         )
-        ax[1].fill_between(
-            tau_series,
-            proj_2norm[k_idx] - proj_2norm_sem[k_idx],
-            proj_2norm[k_idx] + proj_2norm_sem[k_idx],
-            alpha=0.5
-        )
-    ax[0].set(
-        xlabel=r'$\tau$',
-        ylabel=r'$||\rho_\text{Proj.}^{(k)}||_1',
-        title=rf'1-norm'
-    )
-    ax[1].set(
-        xlabel=r'$\tau$',
-        ylabel=r'$||\rho_\text{Proj.}^{(k)}||_2',
-        title=rf'2-norm'
-    )
-    ax[0].legend(**ut.LEGEND_OPTIONS)
-    ax[1].legend(**ut.LEGEND_OPTIONS)
+        ax[p].legend(**ut.LEGEND_OPTIONS)
     fig.suptitle(p_str, y=1.1)
-    fig.savefig(os.path.join(ut.FIG_DIR, f'proj_norm_{exp_note(dr, k_series[-1])}'))
+    fig.savefig(os.path.join(ut.FIG_DIR, f'proj_norm_{exp_note(dr, f"{k_series[0]}-{k_series[-1]}")}.svg'), **ut.FIG_SAVE_OPTIONS)
 
---
+++ projected_funcs.py
@@ -0,0 +1,56 @@
+import string
+
+import numpy as np
+
+import dynamite.computations as comp
+
+import utilities as ut
+
+
+einsum_idxes = list(string.ascii_lowercase)
+
+
+def moment_constructor(k):
+    assert 2*k+2 < len(einsum_idxes)
+    input = ','.join([f'ab{i}' for i in einsum_idxes[2:2*k+2]])
+    output = ''.join(einsum_idxes[2:2*k+2])
+    return input + '->a' + output
+
+
+def norm(arr, p, **kwargs):
+    ord = None
+    if p == 1:
+        ord = 'nuc'
+    elif p == 2:
+        ord = 'fro'
+    else:
+        raise NotImplementedError('Only norms 1 and 2 are implemented')
+    return np.linalg.norm(arr, ord=ord, **kwargs)
+
+
+@ut.cache('npz', 'proj_moment')
+def proj_moment(H, psi0, tau_series, k_series, DA, note=None):
+    psit = [psi0]
+    for i, dt in enumerate(np.ediff1d(tau_series, to_begin=tau_series[0])):
+        psit.append(comp.evolve(H, psit[-1], dt))
+    psit = np.asarray([p.to_numpy(to_all=True) for p in psit[1:]])
+    psit = psit.reshape(tau_series.size, -1, DA)
+    norm = np.sum(np.abs(psit)**2, axis=-1, keepdims=True)
+    out = {}
+    for k_idx, k in enumerate(k_series):
+        # TODO: post-select on parity
+        moment = np.einsum(
+            moment_constructor(k),
+            *((psit / norm**(1-1/k),)*k + (psit.conj(),)*k),
+            optimize=True
+        )
+        out[str(k)] = moment.reshape(
+            tau_series.size,
+            DA**k,
+            DA**k
+        )
+    return out
+
+
+def scr_moment()
+
